dba2cfdc3feec1aec071c720d498f92cc34299c5
dashatyshch <dasha.tyshenko98@gmail.com>
2020-02-02 00:49:58 +0200

    Initial commit

---
+++ src/test/java/com/example/HotelSPP/HotelSPPApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HotelSPP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelSPPApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
