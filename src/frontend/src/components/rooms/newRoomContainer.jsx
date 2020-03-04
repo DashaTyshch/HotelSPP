da90fd85617919c6a999ae28b7bf22401de56c43
dashatyshch <dasha.tyshenko98@gmail.com>
2020-03-04 23:09:06 +0200

    Fixed elements layout in new room page

--- src/frontend/src/components/rooms/newRoomContainer.jsx
+++ src/frontend/src/components/rooms/newRoomContainer.jsx
@@ -2,16 +2,12 @@ import React from "react";
 import connect from "react-redux/es/connect/connect";
 import {Button, TextField} from "@material-ui/core";
 import makeStyles from "@material-ui/core/styles/makeStyles";
+import Grid from "@material-ui/core/Grid";
 
 const useStyles = makeStyles(theme => ({
-    title: {
-        textAlign: "center",
-        color: theme.palette.turquoise.backgroundColor,
-        letterSpacing: "0.009em",
-        fontWeight: "bold",
-    },
     form: {
-        width: '80%',
+        margin: '0 auto',
+        width: '90%',
         '& label.Mui-focused': {
             color: theme.palette.turquoise.backgroundColor,
         },
@@ -19,14 +15,31 @@ const useStyles = makeStyles(theme => ({
             borderBottomColor: theme.palette.turquoise.backgroundColor,
         },
     },
-    submit: {
-        margin: theme.spacing(3, 0, 2),
-        backgroundColor: theme.palette.lightBlue.backgroundColor,
-        color: theme.palette.lightBlue.color,
+    infoBlock: {
+        display: "flex",
+        flexWrap: "wrap",
+        justifyContent: "space-evenly",
+        alignItems: "flex-start",
+        width: "100%",
+    },
+    infoBlockCol: {
+        display: "flex",
+        flexDirection: "column",
+    },
+    button: {
+        margin: theme.spacing(3),
         '&:hover': {
-            backgroundColor: theme.palette.blue.backgroundColor,
+            backgroundColor: theme.palette.lightBlue.backgroundColor,
             color: theme.palette.blue.color
         },
+    },
+    confirmBtn: {
+        backgroundColor: theme.palette.blue.backgroundColor,
+        color: theme.palette.blue.color,
+        // '&:hover': {
+        //     backgroundColor: theme.palette.lightBlue.backgroundColor,
+        //     color: theme.palette.blue.color
+        // },
     },
 }));
 
@@ -36,60 +49,71 @@ function NewRoomContainer(props) {
         <>
             <div>
                 <form className={useStyles().form} noValidate>
-                    <TextField size="normal" margin="normal" required
-                        //fullWidth
-                        id="roomType"
-                        label="Тип номеру"
-                        name="roomType"
-                        autoComplete="off"
-                        autoFocus
-                        //value={phone}
-                        //onChange={(e) => setPhone(e.target.value)}
-                    />
-                    <TextField size="normal" margin="normal"
-                        id="places"
-                        label="Кількість спальних місць"
-                        type="number"
-                               InputProps={{ inputProps: { min: 1} }}
-                        InputLabelProps={{
-                            shrink: true,
-                        }}
-                    />
-                    <TextField size="normal" margin="normal"
-                        id="amount"
-                        label="Загальна кількість номерів"
-                        type="number"
-                               InputProps={{ inputProps: { min: 1} }}
-                        InputLabelProps={{
-                            shrink: true,
-                        }}
-                    />
+                    <Grid container justify="center"
+                          alignItems="center" direction="column">
+                        <TextField size="normal" margin="normal" required
+                                   id="roomType"
+                                   label="Тип номеру"
+                                   name="roomType"
+                                   autoComplete="off"
+                                   autoFocus
+                            //value={phone}
+                            //onChange={(e) => setPhone(e.target.value)}
+                        />
 
-                    <TextField size="normal" margin="normal"
-                        id="description"
-                        label="Опис"
-                        multiline
-                        rows="6"
-                        //defaultValue="Default Value"
-                        variant="outlined"
-                    />
+                        <div className={useStyles().infoBlock}>
+                            <div className={useStyles().infoBlockCol}>
+                                <TextField size="normal" margin="normal"
+                                           id="description"
+                                           label="Опис"
+                                           multiline
+                                           rows="6"
+                                    //defaultValue="Default Value"
+                                           variant="outlined"
+                                />
+                                <TextField size="normal" margin="normal" required
+                                           id="places"
+                                           label="Кількість спальних місць"
+                                           type="number"
+                                           InputProps={{ inputProps: { min: 1} }}
+                                           InputLabelProps={{
+                                               shrink: true,
+                                           }}
+                                />
+                            </div>
 
-                    <Button
-                        //type="submit"
-                        //fullWidth
-                        variant="contained"
-                        className={useStyles().submit}
-                    >
-                        Скасувати
-                    </Button>
-                    <Button
-                        type="submit"
-                        //fullWidth
-                        variant="contained"
-                        className={useStyles().submit}
-                    >
-                        Створити
-                    </Button>
+                            <TextField size="normal" margin="normal" required
+                                       id="amount"
+                                       label="Загальна кількість номерів"
+                                       type="number"
+                                       InputProps={{ inputProps: { min: 1} }}
+                                       InputLabelProps={{
+                                           shrink: true,
+                                       }}
+                            />
+                        </div>
+
+                        <div>
+                            <Button
+                                //type="submit"
+                                //fullWidth
+                                variant="contained"
+                                color="secondary"
+                                className={useStyles().button}
+                            >
+                                Скасувати
+                            </Button>
+
+                            <Button
+                                type="submit"
+                                //fullWidth
+                                variant="contained"
+                                className={`${useStyles().button} ${useStyles().confirmBtn}`}
+                            >
+                                Створити
+                            </Button>
+                        </div>
+                    </Grid>
                 </form>
             </div>
         </>
