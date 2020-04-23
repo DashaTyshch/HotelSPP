6b24eb9525bb7f2363dc3b63828b474e3b21a2db
mikkicon <mikkicon@gmail.com>
2020-04-23 22:15:47 +0300

    Order Repository implementation

--- src/main/java/com/example/HotelSPP/repository/implementation/OrderRepositoryImpl.java
+++ src/main/java/com/example/HotelSPP/repository/implementation/OrderRepositoryImpl.java
@@ -1,18 +1,26 @@
 package com.example.HotelSPP.repository.implementation;
 
 import com.example.HotelSPP.entity.Order;
+import com.example.HotelSPP.exceptions.ResourceNotFoundException;
 import com.example.HotelSPP.repository.interfaces.OrderRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.Date;
-import java.util.List;
-import java.util.Optional;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Types;
+import java.util.*;
 
 @Slf4j
 @Repository
@@ -59,36 +67,131 @@ public class OrderRepositoryImpl implements OrderRepository {
 
     @Override
     public boolean ordersAvailable(long id) {
-        return false;
+        return !findOrderById(id).isPresent();
     }
 
     @Override
     public Order addOrder(Order order) {
-        return null;
+        Map<String, Object> paramMap = new HashMap<>();
+        paramMap.put(paramOrdersDateCreated, order.getDate_created());
+        paramMap.put(paramOrdersGuestId, order.getGuest_Id());
+        paramMap.put(paramOrdersState, order.getState_Id());
+        namedTemplate.update(ADD_ORDER, paramMap);
+
+        Optional<Order> createdOrder = findOrderById(order.getId());
+        return createdOrder.orElseThrow(
+                () -> new ResourceNotFoundException("Orders", "name", order.getId()));
+
     }
 
     @Override
     public Optional<Order> findOrderById(long id) {
-        return Optional.empty();
+        Order res;
+        try {
+            res = namedTemplate.queryForObject(GET_ORDER_BY_ID, new MapSqlParameterSource(
+                    paramOrdersId, id), new OrderMapper());
+        } catch (EmptyResultDataAccessException e) {
+            log.warn(String.format("Couldn't find order by id: %s", id));
+            res = null;
+        } catch (DataAccessException e) {
+            log.error("Error: ", e);
+            throw e;
+        }
+        return Optional.ofNullable(res);
     }
 
     @Override
     public Optional<Order> findOrderByState(String state) {
-        return Optional.empty();
+        Order res;
+        try {
+            res = namedTemplate.queryForObject(GET_ORDER_BY_STATE, new MapSqlParameterSource(
+                    paramOrdersState, state), new OrderMapper());
+        } catch (EmptyResultDataAccessException e) {
+            log.warn(String.format("Couldn't find order by state: %s", state));
+            res = null;
+        } catch (DataAccessException e) {
+            log.error("Error: ", e);
+            throw e;
+        }
+        return Optional.ofNullable(res);
     }
 
     @Override
     public Optional<Order> findOrdersByDate(Date date) {
-        return Optional.empty();
+        Order res;
+        try {
+            res = namedTemplate.queryForObject(GET_ORDER_BY_DATE, new MapSqlParameterSource(
+                    paramOrdersState, date), new OrderMapper());
+        } catch (EmptyResultDataAccessException e) {
+            log.warn(String.format("Couldn't find order by date: %s", date));
+            res = null;
+        } catch (DataAccessException e) {
+            log.error("Error: ", e);
+            throw e;
+        }
+        return Optional.ofNullable(res);
     }
 
     @Override
     public Optional<Order> updateOrder(Order o) {
+        Order current;
+        String updateSql = "UPDATE orders SET ";
+        Optional<Order> db_res;
+        try {
+            db_res = findOrderById(o.getId());
+            if (db_res.isPresent()) {
+
+                ArrayList<Object> params = new ArrayList<>();
+                ArrayList<Integer> types = new ArrayList<>();
+                current = db_res.get();
+                if (!o.getDate_created().equals(current.getDate_created())){
+                    updateSql += " date_created= " + o.getDate_created();
+                    params.add(o.getDate_created());
+                    types.add(Types.DATE);
+                }
+                if (o.getGuest_Id() != (current.getGuest_Id())) {
+                    updateSql += " guest_id= " + o.getGuest_Id() + ", ";
+                    params.add(o.getGuest_Id());
+                    types.add(Types.INTEGER);
+                }
+                if (o.getState_Id() != (current.getState_Id())) {
+                    updateSql += " state_id= " + o.getState_Id() + ", ";
+                    params.add(o.getState_Id());
+                    types.add(Types.INTEGER);
+                }
+                updateSql += " WHERE id =" + current.getId();
+
+                int rows = namedTemplate.update(updateSql,(SqlParameterSource) params,(KeyHolder) types);
+                System.out.println(rows + " row(s) updated.");
+                db_res = findOrderById(o.getId());
+                return db_res;
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Optional.empty();
+        }
         return Optional.empty();
     }
 
     @Override
     public List<Order> getAllOrders() {
-        return null;
+        try {
+            return Collections.unmodifiableList(namedTemplate.query(GET_ALL_ORDERS, new OrderMapper()));
+        } catch (DataAccessException e) {
+            log.error("Error: ", e);
+            throw e;
+        }
+    }
+    private class OrderMapper implements RowMapper<Order> {
+        @Override
+        public Order mapRow(ResultSet rs, int arg1) throws SQLException {
+            return Order.builder()
+                    .id(rs.getInt(paramOrdersId))
+                    .date_created(rs.getDate(paramOrdersDateCreated))
+                    .guest_Id(rs.getLong(paramOrdersGuestId))
+                    .state_Id(rs.getInt(paramOrderStateId))
+                    .build();
+        }
     }
 }
