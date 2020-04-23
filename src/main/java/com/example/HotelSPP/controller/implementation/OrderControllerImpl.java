4a46650442dfcc227a02a8c27f0d4c10c228a234
dashatyshch <dasha.tyshenko98@gmail.com>
2020-04-23 21:46:45 +0300

    Commit fix "Order Service backbone"

--- src/main/java/com/example/HotelSPP/controller/implementation/OrderControllerImpl.java
+++ src/main/java/com/example/HotelSPP/controller/implementation/OrderControllerImpl.java
@@ -16,7 +16,7 @@ import java.util.List;
 
 @Slf4j
 @RestController
-@RequestMapping("/api/room_type")
+@RequestMapping("/api/order")
 public class OrderControllerImpl implements OrderController {
     @Autowired
     private OrderService service;
